ff1e25c22ad4fd96370f158b2d4230ce6225b977
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 14:17:34 +0200

    Applied fixes from StyleCI (#3)
    [ci skip] [skip ci]

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Parsedown.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Parsedown;
 
 use Illuminate\Support\ServiceProvider as IlluminateProvider;
